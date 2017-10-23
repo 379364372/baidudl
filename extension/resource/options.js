f8963a8b96f3c84a6151eb091228584f14e7058a
Frye Lee <fyl00lee@gmail.com>
2017-10-24 00:06:46 +0800

    beautify page.

--- extension/resource/options.js
+++ extension/resource/options.js
@@ -5,22 +5,21 @@ $(function(){
                 var self =this;
                 var rpc_list=JSON.parse(localStorage.getItem("rpc_list")||'[{"name":"Aria2","url":"http://localhost:6800/jsonrpc"}]');
                 for(var i in rpc_list){
-                    var addBtn=0==i?'<button class="btn" id="add-rpc">Add RPC</button>':'';
-                    var row='<div class="control-group rpc_list"><label class="control-label">JSON-RPC</label><div class="controls"><input type="text" class="input-small" value="'+rpc_list[i]['name']+'" placeholder="RPC Name"><input type="text" class="input-xlarge rpc-path" value="'+rpc_list[i]['url']+'" placeholder="RPC Path">'+addBtn+'</div></div>';
-                    if($(".rpc_list").length>0){
-                        $(row).insertAfter($(".rpc_list").eq(i-1));
+                    var row='<div class="control-group rpc_details"><label class="control-label">JSON-RPC</label><div class="controls"><input type="text" class="input-small rpc-name" value="'+rpc_list[i]['name']+'" placeholder="RPC Name"><input type="text" class="input-xlarge rpc-path" value="'+rpc_list[i]['url']+'" placeholder="RPC Path"></div></div>';
+                    if($(".rpc_details").length>0){
+                        $(row).insertAfter($(".rpc_details").eq(i-1));
                     }else{
                         $(row).insertAfter($("fieldset legend"));
                     }
                 }
 
                 $("#add-rpc").on("click",function(){
-                    var rpc_form='<div class="control-group rpc_list">'+
+                    var rpc_form='<div class="control-group rpc_details">'+
                         '<label class="control-label">JSON-RPC</label>'+
                         '<div class="controls">'+
-                          '<input type="text" class="input-small"  placeholder="RPC Name">'+
+                          '<input type="text" class="input-small rpc-name"  placeholder="RPC Name">'+
                           '<input type="text" class="input-xlarge rpc-path"  placeholder="RPC Path"></div></div>';
-                    $(rpc_form).insertAfter($(".rpc_list")[0]);
+                    $("#rpc-list").append(rpc_form);
                 });
                 $("#save").on("click",function(){
                     self.save();
@@ -32,8 +31,8 @@ $(function(){
             },
             save:function(){
                 var rpc_list=[];
-                for(var i=0;i<$(".rpc_list").length;i++){
-                    var child=$(".rpc_list").eq(i).children().eq(1).children();
+                for(var i=0;i<$(".rpc_details").length;i++){
+                    var child=$(".rpc_details").eq(i).children().eq(1).children();
                     if(child.eq(0).val()!= ""&&child.eq(1).val()!= ""){
                         rpc_list.push({"name":child.eq(0).val(),"url":child.eq(1).val()});
                     }
