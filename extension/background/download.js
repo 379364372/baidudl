b39f72e0b9fff6a316766046a36b8f0e4f629b50
Kyle ZENG <jkjh1jkjh1@gmail.com>
2018-03-11 12:42:16 +0800

    [wip] filter out false hlinks

--- extension/background/download.js
+++ extension/background/download.js
@@ -15,6 +15,26 @@ function DownloadManager(glink)
         config.servers = config.servers.concat(additional);
         chrome.storage.local.set({'config': config});
     };
+    self.filterHLinks = function(hlinks, cb){
+        var filtered = [];
+        var promises = hlinks.map(function(e){
+            var promise = $.ajax({
+                url: e,
+                type: 'HEAD',
+                timeout: 3000,
+                success: function(res, status, request){
+                    if(request.getResponseHeader('Content-MD5')){
+                        filtered.push(e);
+                    }
+                }
+            });
+            return promise;
+        });
+        Q.allSettled(promises).then(function(res){
+            console.log(filtered);
+            cb(filtered);
+        });
+    };
     self.getHLinks = function(cb){
         var parsed_glink = self.parsed_glink;
         if(!page.bduss){
@@ -28,17 +48,16 @@ function DownloadManager(glink)
         }
         var pathnames = parsed_glink.pathname.split('/');
         var url = 'https://d.pcs.baidu.com/rest/2.0/pcs/file?time='+parsed_glink.searchParams.get('time')+'&version=2.2.0&vip=1&path='+pathnames[pathnames.length-1]+'&fid='+parsed_glink.searchParams.get('fid')+'&rt=sh&sign='+parsed_glink.searchParams.get('sign')+'&expires=8h&chkv=1&method=locatedownload&app_id=250528&esl=0&ver=4.0';
-        console.log(url);
         $.ajax({
             url: url,
             dataType: 'json',
             success: function(res){
-                console.log(res);
                 if(res.error_code && res.error_code != 0){
                     new Error(res.error_code).handle();
                     return;
                 }
                 console.log('Get hlink list success');
+                console.log(res.urls);
                 var hlinks = res.urls.map(function(e){
                     return e.url;
                 });
@@ -49,7 +68,9 @@ function DownloadManager(glink)
         });
     };
     self.download = function(){
-        self.getHLinks(self.__download__);
+        self.getHLinks(function(hlink){
+            self.filterHLinks(hlink, self.__download__);
+        });
     };
     self.__download__ = function(hlinks){
         // prepare json request
